53956fa59af2be83feff9dadde9889b52c52163e
Mukund Jagani <137892321+mukund2506@users.noreply.github.com>
2025-08-15 12:17:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -107,3 +107,23 @@ closeModal.addEventListener('click', () => {
     modal.style.display = 'none';
     modalContent.innerHTML = '';
 });
+// Get the button
+const goTopBtn = document.getElementById("goTopBtn");
+
+// Show the button when user scrolls down 200px
+window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        goTopBtn.style.display = "block";
+    } else {
+        goTopBtn.style.display = "none";
+    }
+};
+
+// Scroll to top smoothly when clicked
+goTopBtn.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
